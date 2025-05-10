b661e3933233bebcae6535a3ab71309d544e01f7
Jacob Edwards <jacob.john1@talktalk.net>
2025-05-10 15:15:35 +0100

    main

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,17 @@
 module Main (main) where
 
+class Assignment where
+    empty :: Assignment
+    assign :: Assignment -> Literal -> Bool -> Assignment
+    isAssigned :: Assignment -> Literal -> Maybe Bool
+    isComplete :: Assignment -> Bool
+    getUnassigned :: Assignment -> [Literal]
+
+data Solver = Solver {
+    clauses :: [Clause],
+    assignment :: Assignment,
+}
+
 
 main :: IO ()
 main = undefined
